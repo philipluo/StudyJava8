8b0073f9404e3ec46fc74757b55a047c86297940
philipl <philipl@synnex.com>
2018-04-11 21:59:33 +0800

    study java IO

---
+++ src/main/java/com/philip/studyIO/ReadFileTest.java
@@ -0,0 +1,62 @@
+package com.philip.studyIO;
+
+import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+public class ReadFileTest {
+
+    /**
+     * Study from http://www.baeldung.com/reading-file-in-java
+     * 1. read file from classpath
+     * 2. read file with JDK7
+     * 3. read file with JDK8
+     * 4. read file with FileUtils
+     * 5. Read Content from URL
+     * 6. Read File from JAR
+     */
+    @Test
+    public void readFileTest() {
+        String fileName = "sub/a.json";
+
+        try {
+            //Read File from JAR
+            System.out.println("Method 1: bufferReader.readLine()");
+            InputStream is = ReadFileTest.class.getClassLoader().getResourceAsStream(fileName);
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(is));
+            String line = null;
+            while ((line = bufferedReader.readLine()) != null) {
+                System.out.println(line);
+            }
+            bufferedReader.close();
+
+            //mehtod 2
+            System.out.println("Method 2: bufferReader.lines()");
+            InputStream is2 = ReadFileTest.class.getClassLoader().getResourceAsStream(fileName);
+            BufferedReader bufferedReader2 = new BufferedReader(new InputStreamReader(is2));
+            bufferedReader2.lines().forEach(System.out::println);
+            bufferedReader2.close();
+
+
+            //method 3
+            System.out.println("Method 3: Files.lines");
+            Path path = Paths.get(getClass().getClassLoader().getResource(fileName).toURI());
+            Stream<String> lines = Files.lines(path);
+            lines.forEach(System.out::println);
+            lines.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
